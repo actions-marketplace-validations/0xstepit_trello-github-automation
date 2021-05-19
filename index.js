a96b48edb7fc18efd5582fbb43902310a0d82435
Matt Taylor <twenty7@gmail.com>
2021-05-19 15:19:48 -0700

    Add Debugging

--- index.js
+++ index.js
@@ -161,7 +161,9 @@ function changeCardWhenIssueEdited(apiKey, apiToken, boardId, memberMap) {
           let cardId;
           let existingMemberIds = [];
           let cardData;
+          console.log('Github Payload', github.context.payload);
           cards.some(function (card) {
+            console.log('Card Check', card);
             if (card.name == `[#${number}] ${targetTitle}`) {
               cardId = card.id;
               existingMemberIds = card.idMembers;
